9854f57bf74a4603667bcef9caf135883a93d954
AdrsP <144963769+AdrsP@users.noreply.github.com>
2023-11-22 01:11:54 +0000

    mejor falta darle estilo

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -2,38 +2,36 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 
-const list = ["mono","mesa"];
-
 const ToDoList = () => {
 
     const [valorInput, setValorInput] = useState("");
-
-
-    function cambiandoList (event) {
-        if (event.key == "Enter"){
-        list.push(valorInput);
-        console.log(list);
-        };
-    };
+    const [list, setList] = useState([]);
 
     return (
         <div>
-            <input type="text" name="" id="" value={valorInput} onKeyDown={e => cambiandoList(e)} onChange={e => setValorInput(e.target.value)}/>
             <ul>
-                <li>{valorInput}</li>
+                <li>
+                    <input type="text" name="" placeholder="Holo" id="" value={valorInput} 
+                    onKeyDown={e => (e.key == "Enter") ? (setList(list.concat(valorInput)), setValorInput("")): null } 
+                    onChange={e => setValorInput(e.target.value)}/>
+                </li>
+                {list.map((listItem,listIndex) => (
+                    <li className="elmentolista">{listItem}{" "}
+                    <i 
+                        className="fa-solid fa-x icono" 
+                        onClick={() => 
+                            setList(
+                                list.filter(
+                                    (relleno, currentIndex) => listIndex != currentIndex
+                                )
+                            )      
+                            }>
+                        </i> 
+                    </li>
+                ))}
             </ul>
         </div>
     );
 };
 
 export default ToDoList;
-
-//
-const animals = ["Horse", "Turtle", "Elephant", "Monkey", "jose"];
-
-/**
- * change the content inside the map function
- **/
-const animalsInHTML = animals.map((singleAnimal, i) => {
-	return <li key={i}>{singleAnimal}</li>;
-});
